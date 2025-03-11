00540a5a555a5a3193edd4f7ec4c766219139164
Faysal Ahmed <kawsar.ahmed042@gmail.com>
2025-03-11 14:58:02 +0400

    removed nav

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,10 +1,8 @@
 import { fontClassName } from "../config/fonts";
-import Navbar from "../components/Navbar";
 
 export default function Blog() {
   return (
     <div className={fontClassName}>
-      <Navbar />
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <h1 className="text-2xl font-bold mb-6">Coming Soon</h1>
       </main>
